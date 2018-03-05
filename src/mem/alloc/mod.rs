392e19be3716d8f5b76fde0de0f42aa408a142a3
Grant Jurgensen <gajurgensen@gmail.com>
2018-03-04 19:09:08 -0600

    box deletes heap memory when it drops out of scope

--- src/mem/alloc/mod.rs
+++ src/mem/alloc/mod.rs
@@ -4,7 +4,7 @@ pub mod first_fit;
 use core::intrinsics::size_of;
 use mem::HEAP_ALLOC;
 
-pub struct Box<T: ?Sized>{
+pub struct Box<T>{ //<T: ?Sized> ?
     elem: *mut T,
 }
 
@@ -19,6 +19,7 @@ impl<T> Box<T> {
     pub fn deref(&self) -> &T {
         unsafe { &*self.elem }
     }
+    // Should manual deletion be allowed, or just let Drop take care of it?
     pub fn del(&self) {
         unsafe {
             HEAP_ALLOC.free(self.elem as u32);
@@ -26,22 +27,9 @@ impl<T> Box<T> {
     }
 }
 
-// Box is a smart pointer that dynamically allocates memory
-// pub struct Box<T: ?Sized>(core::ptr::Unique<T>);
-//
-// impl<T> Box<T> {
-//     fn new(x: T) -> Box<T> {
-//         let addr = //ff.alloc(size_of(x));
-//         //copy over??
-//         Box(core::ptr::Unique::new_unchecked(1 as *mut T))
-//     }
-// }
-
-// Deref trait is in std lib, look into overloading operations manually?
-// impl<T> Deref for Box<T> {
-//     type Target = T;
-//
-//     fn deref(&self) -> &T {
-//         &self.0
-//     }
-// }
+// This overload of the drop function calls our delete function when a Box drops out of scope
+impl<T> Drop for Box<T> {
+    fn drop(&mut self) {
+        self.del();
+    }
+}
